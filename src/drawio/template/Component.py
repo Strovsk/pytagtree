52ff1e01e25d3c292f6f8d5ad1f8fcbcb14adfb8
Strovsk <strovsk@outlook.com>
2022-05-18 11:00:22 -0300

    Component template added

---
+++ src/drawio/template/Component.py
@@ -0,0 +1,50 @@
+import re
+
+class Component:
+    def __init__(self, treeId, indentation = 0):
+        self.children = []
+        self.__contentBase = 'Component ' + str(treeId)
+        # self.__content = self.__contentBase
+        self.__content = ''
+        self.__indentation = indentation
+    
+    def push(self, item):
+        item.addIndentation(self.__indentation)
+        self.children.append(item)
+    
+    def __renderIndentation(self):
+        return ''.join(['  ' for i in range(self.__indentation)])
+    
+    def addIndentation(self, fatherIndentation):
+        self.__indentation = fatherIndentation + 1
+        if self.children == 0: return
+        for child in self.children:
+            child.addIndentation(self.__indentation)
+        
+    def removeIndentation(self, fatherIndentation):
+        self.__indentation = fatherIndentation - 1
+        if self.children == 0: return
+        for child in self.children:
+            child.removeIndentation(self.__indentation)
+    
+    def genContent(self):
+        if len(self.children) == 0:
+            indent = self.__renderIndentation()
+            return (
+                re.sub(
+                    r'^(.*)',
+                    f'{indent}<\g<1> />',
+                    self.__contentBase,
+                    0,
+                    re.M
+                )
+            )
+        buffer = ''
+        for child in self.children:
+            buffer += (
+                '\n' +
+                child.genContent()
+            )
+        firstTagPart = f'{self.__renderIndentation()}<{self.__contentBase}>'
+        endTag = f'\n{self.__renderIndentation()}</{self.__contentBase}>'
+        return firstTagPart + buffer + endTag
