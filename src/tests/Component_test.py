2071edd6df9a965a764e062171c6ee7fea33e2ec
Strovsk <strovsk@outlook.com>
2022-05-18 16:50:33 -0300

    Component tests changeds

--- src/tests/Component_test.py
+++ src/tests/Component_test.py
@@ -9,16 +9,16 @@ from drawioartisan.template.Component import Component
 class ComponentTest(unittest.TestCase):
     def test_returnSelfClosedWithOneChildren(self):
         # Should return right format when dont have children
-        comp = Component('1', 0)
-        expectedComponent = '<Component 1 />'
+        comp = Component('ComponentTag', 0)
+        expectedComponent = '<ComponentTag/>'
         self.assertEqual(comp.genContent(), expectedComponent)
     
     def test_rightFormat(self):
         # Should return right format when have chlidren
-        comp1 = Component('1')
-        comp2 = Component('2')
+        comp1 = Component('Component1')
+        comp2 = Component('Component2')
         comp1.push(comp2)
-        expectedComponentTree = '<Component 1>\n  <Component 2 />\n</Component 1>'
+        expectedComponentTree = '<Component1>\n  <Component2/>\n</Component1>'
         self.assertEqual(comp1.genContent(), expectedComponentTree)
     
     def test_indentationUpdate(self):
