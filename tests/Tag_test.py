ca4f9e7a496f7bef9de3d257100b7ec25426e710
Strovsk <strovsk@outlook.com>
2022-05-25 17:12:27 -0300

    tests and models

--- tests/Tag_test.py
+++ tests/Tag_test.py
@@ -5,100 +5,100 @@ import unittest
 sys.path.append(os.path.abspath('../src'))
 from TagTree.template.Tag import Tag
 
+def getContentModel():
+    path = f'./helpers/tagFileModel.model.html'
+    file = open(path, 'r')
+    data = [f[:-1] for f in file.readlines()]
+    file.close()
+    return data
+
+def digestTestName(testName):
+    replaced = testName.replace('_', ' ')
+    return  replaced[0].upper() + replaced[1:]
+
 class TagTest(unittest.TestCase):
-    def test_returnFormattedTag(self):
-        # Should return the right formatted string when line is not greater than limit 80
-        lineLimit = 80
-        main = Tag(
-            'MainTag',
-            'idMainTag',
-            [
-                ('param1', 'value1'),
-                ('param2', 'value2'),
-                ('param3', 'value3')
-            ],
-            lineLimit,
-            2
+    def test_closed_tag_have_no_slash_id_1(self):
+        '''Self closed Tag have right format (only one no value param, hidden id, no slash)'''
+        data = getContentModel()[0]
+        tagTest = Tag(
+            '!DOCTYPE',
+            'doctypeId',
+            ['html'],
+            90,
+            2,
+            '',
+            True,
+            True
         )
-        expected = '<MainTag id="idMainTag" param1="value1" param2="value2" param3="value3" />'
-        self.assertEqual(main.genContent(), expected)
-
-        # Should Break Line the line is greater than limit
-        lineLimit = 50
-        main = Tag(
-            'MainTag',
-            'idMainTag',
-            [
-                ('param1', 'value1'),
-                ('param2', 'value2'),
-                ('param3', 'value3')
-            ],
-            lineLimit,
-            2
-        )
-        expected = '<MainTag\n  id="idMainTag"\n  param1="value1"\n  param2="value2"\n  param3="value3"\n/>'
-        self.assertEqual(main.genContent(), expected)
-    
-    def test_returnRightModelWithChildrenNodes(self):
-        # Should return the right formatted string when line is not greater than limit 80
-        lineLimit = 80
-        main = Tag(
-            'MainTag',
-            'idMainTag',
-            [
-                ('param1', 'value1'),
-                ('param2', 'value2'),
-                ('param3', 'value3')
-            ],
-            lineLimit,
-            2
-        )
-        child = Tag(
-            'ChildTag',
-            'idChildTag',
-            [
-                ('param1', 'value1'),
-                ('param2', 'value2'),
-                ('param3', 'value3')
-            ],
-            lineLimit,
-            2
-        )
-        main.push(child)
-        expected = '<MainTag id="idMainTag" param1="value1" param2="value2" param3="value3" >\n  <ChildTag id="idChildTag" param1="value1" param2="value2" param3="value3" />\n</MainTag>'
-        self.assertEqual(main.genContent(), expected)
-
-        # Should Break Line the line is greater than limit
-        lineLimit = 50
-        main = Tag(
-            'MainTag',
-            'idMainTag',
-            [
-                ('param1', 'value1'),
-                ('param2', 'value2'),
-                ('param3', 'value3')
-            ],
-            lineLimit,
-            2
-        )
-        child = Tag(
-            'ChildTag',
-            'idChildTag',
-            [
-                ('param1', 'value1'),
-                ('param2', 'value2'),
-                ('param3', 'value3')
-            ],
-            lineLimit,
-            2
-        )
-        main.push(child)
-        expected = '<MainTag\n  id="idMainTag"\n  param1="value1"\n  param2="value2"\n  param3="value3"\n>\n  <ChildTag\n    id="idChildTag"\n    param1="value1"\n    param2="value2"\n    param3="value3"\n  />\n</MainTag>'
-
-        self.assertEqual(main.genContent(), expected)
-
-    def test_returnRightSpaceAtEnd(self):
+        self.assertEqual(tagTest.genContent(), data)
         pass
 
+    def test_closed_tag_have_no_slash_id_2(self):
+        '''Self closed Tag have right format (slash, value params and no value params)'''
+        data = getContentModel()[12][4:]
+        tagTest = Tag(
+            'img',
+            'imgId',
+            [
+                ('src', '../../Assets/Logo.png'),
+                ('alt', 'TagTree Logo'),
+                'fakeParamA',
+                'fakeParamB' 
+            ],
+            255,
+            2,
+            '',
+            False,
+            True
+        )
+        self.assertEqual(tagTest.genContent(), data)
+        pass
+    
+    def test_closed_tag_have_no_slash_id_3(self):
+        '''Self closed Tag have right format (empty tag)'''
+        data = getContentModel()[11][2:]
+        tagTest = Tag(
+            'body',
+            'bodyId',
+            [],
+            255,
+            2,
+            '',
+            True,
+            True
+        )
+        self.assertEqual(tagTest.genContent(), data)
+        pass
+
+    def test_closed_tag_have_no_slash_id_4(self):
+        '''Self closed Tag have right format (only slash)'''
+        data = getContentModel()[18][4:]
+        tagTest = Tag(
+            'input',
+            'inputId',
+            [],
+            255,
+            2,
+            '',
+            False,
+            True
+        )
+        self.assertEqual(tagTest.genContent(), data)
+        pass
+
+    def load_tests(self):
+        buffer = []
+        for method in dir(self):
+            if method.find('test_') != -1:
+                buffer.append(method)
+        return buffer
+
+def suite():
+    s = unittest.TestSuite()
+    for test in TagTest().load_tests():
+        s.addTest(TagTest(test))
+    return s
+
 if __name__ == '__main__':
-    unittest.main()
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
